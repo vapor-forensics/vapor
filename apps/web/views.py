0d8ed9eb36e2dc13a4e695ef14676e8982131d56
vapor-forensics <vaporforensics@gmail.com>
2025-01-06 15:03:16 +1000

    ready to upgrade

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.http import Http404
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
+from health_check.views import MainView
 
 
 def home(request):
@@ -18,3 +21,11 @@ def home(request):
 
 def simulate_error(request):
     raise Exception("This is a simulated error.")
+
+
+class HealthCheck(MainView):
+    def get(self, request, *args, **kwargs):
+        tokens = settings.HEALTH_CHECK_TOKENS
+        if tokens and request.GET.get("token") not in tokens:
+            raise Http404
+        return super().get(request, *args, **kwargs)
